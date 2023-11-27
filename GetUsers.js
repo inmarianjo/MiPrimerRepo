f597439f7de5942765c66d06f62ded29ed978651
Harold Aparicio <haparicio@ubits.co>
2023-11-27 17:26:17 -0500

    Se agregan nuevas librerías

--- GetUsers.js
+++ GetUsers.js
@@ -1,15 +1,15 @@
-"use strict";
-const https = require("https");
+var https = require("https");
 https
-    .get("https://reqres.in/api/users?page=2", (resp) => {
-    let data = "";
-    resp.on("data", chunk => {
+    .get("https://reqres.in/api/users?page=2", function (resp) {
+    var data = "";
+    resp.on("data", function (chunk) {
         data += chunk;
     });
-    resp.on("end", () => {
+    resp.on("end", function () {
         console.log(JSON.parse(data));
     });
 })
-    .on("error", (err) => {
+    .on("error", function (err) {
     console.log("Error: " + err.message);
 });
+// algo//
